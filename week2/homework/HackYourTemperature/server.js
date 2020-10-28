83ed9ddf4096df3a21c1412998fd38e5958f2264
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-10-28 13:28:09 +0100

    Finished updating the HackYourTemp app

--- week2/homework/HackYourTemperature/server.js
+++ week2/homework/HackYourTemperature/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-//const API_KEY = require('./sources/keys.json').API_KEY;
+const API_KEY = "a54b947f262e2c5b975e64258dc64f90";
 //initialize express
 const app = express();
 
@@ -21,18 +21,15 @@ app.use(express.urlencoded({ extended: true }));
 
 //use handlebars as a template to render the template
 app.get('/', function(req, res){
-    //res.send('hello from backend to frontend!');
-    console.log('running on port 3000');
     res.render('index');
 })
 
 app.post('/weather', function(req, res){
   const cityName = req.body.cityName;
-  const API_KEY = "a54b947f262e2c5b975e64258dc64f90";
 
   axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${API_KEY}`)
   .then((response) => {
-    let temp = response.data.main.temp;
+    let temp = Math.floor(response.data.main.temp);
     res.render('index', { weatherText: `The temperature in ${cityName} is ${temp}C!` })
   })
   .catch((err) => {
@@ -41,5 +38,5 @@ app.post('/weather', function(req, res){
   })
 })
 
-//listen to port 3000
+//listen to port 3000 to open on browser
 app.listen(3000);
